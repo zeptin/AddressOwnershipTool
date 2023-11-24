8282529f6570320e35ccbdc8f10e864436f88845
Igor Goldobin <igor.goldobin@spendconsole.com>
2023-11-24 18:53:13 +1000

    fix: for sqlite repo

--- AddressOwnershipTool/Commands/Validate/ValidateCommandValidator.cs
+++ AddressOwnershipTool/Commands/Validate/ValidateCommandValidator.cs
@@ -5,7 +5,7 @@ namespace AddressOwnershipTool.Commands.Validate;
 public class ValidateCommandValidator : AbstractValidator<ValidateCommand>
 {
     public ValidateCommandValidator()
-    {            
+    {
         RuleFor(x => x.Signaturefolder).NotEmpty().WithName("--sigfolder");
     }
 }
